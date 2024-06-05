4651b7ced2a50ddcaf4833fe0d8b79ee300f5ecc
MaleekaA <154966150+MaleekaA@users.noreply.github.com>
2024-06-05 22:02:02 +0900

    Update api.py

--- api/api.py
+++ api/api.py
@@ -50,4 +50,5 @@ def nearest_locations():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
