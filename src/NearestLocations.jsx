389ab8371a6f7563c2752688b0636d22c014dde6
MaleekaA <154966150+MaleekaA@users.noreply.github.com>
2024-06-05 11:21:37 +0900

    Update NearestLocations.jsx

--- src/NearestLocations.jsx
+++ src/NearestLocations.jsx
@@ -10,7 +10,7 @@ function NearestLocations() {
   const handleSearch = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`http://127.0.0.1:5000/api/route`, {
+      const response = await fetch(`https://travel-cpun.onrender.com/api/route`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
