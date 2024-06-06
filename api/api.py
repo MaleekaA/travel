8eb3593dd521bb5e14ea1d757e5fab849e620668
Maleeka <maleekakhan19@gmail.com>
2024-06-06 13:10:15 +0900

    url changes and update package .json

--- api/api.py
+++ api/api.py
@@ -49,5 +49,5 @@ def nearest_locations():
     except ValueError as e:
         return jsonify({'error': str(e)}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
