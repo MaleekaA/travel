c22e1a10efd633136772f248e39e19ab88aea654
Maleeka <maleekakhan19@gmail.com>
2024-06-06 12:35:15 +0900

    Updates

---
+++ precache-manifest.4a938e4a72e9e455520eb2f2256175b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a19cd205611ee536d5163c5bc908b9",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "03253e789a2905aae8ad",
+    "url": "/myapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/myapp/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/myapp/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "03253e789a2905aae8ad",
+    "url": "/myapp/static/js/main.370b4131.chunk.js"
+  },
+  {
+    "revision": "c768ba54b81b82e8a4c3",
+    "url": "/myapp/static/js/runtime~main.2342fc8e.js"
+  }
+]);
